6d7631f095289403b337fdf87c024425f329f333
Romulo Soares <37163123+romulolassoares@users.noreply.github.com>
2021-01-28 21:22:22 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,13 @@ function buttonLamp(){
         lamapada.src = "https://romulolassoares.github.io/arduino/assets/images/lamp2.svg";
         lampadaStatus = 0;
         localStorage.setItem("lampadaStatus", lampadaStatus);
-        window.location.href = "/?desligar";
+        window.location.href = "/?desliga";
     } else if (estado === 'Ligar') {
         botao.value = "Desligar";
         lamapada.src = "https://romulolassoares.github.io/arduino/assets/images/lamp.svg"
         lampadaStatus = 1;
         localStorage.setItem("lampadaStatus", lampadaStatus);
-        window.location.href = "/?ligar";
+        window.location.href = "/?liga";
     }
     verifyLampStatus()
 }
